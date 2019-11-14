e3286c6eb256f272b6ff38787076ee5a2fe80d78
Mark Davis Blasko <mblasko@g.clemson.edu>
2019-11-14 15:03:29 -0500

    Initial commit

---
+++ src/main/java/com/tlc/tlc/TlcApplication.java
@@ -0,0 +1,13 @@
+package com.tlc.tlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlc/tlc/TlcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlc.tlc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
